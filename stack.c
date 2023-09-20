87c5d89cb25356bd1a0e60b4c14f223f2c3a7e80
Mazen Ayoub <105595088+Mazen-Ayub@users.noreply.github.com>
2023-09-20 05:03:01 -0700

    monty

---
+++ stack.c
@@ -0,0 +1,15 @@
+#include "monty.h"
+
+/**
+ * f_stack - a function that prints the top
+ * @head: the stack head
+ * @counter: the line_number
+ * Return: nothing
+*/
+
+void f_stack(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+	bus.lifi = 0;
+}
